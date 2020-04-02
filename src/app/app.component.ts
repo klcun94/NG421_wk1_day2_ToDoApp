4cab70200b88c4f6cd243895428c67d4fa6f9e70
klcun94 <klcun94@icloud.com>
2020-04-01 20:20:38 -0500

    it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'Todos';
   todoList: any [] = [];
+  todoTitle: string;
 
   ngOnInit() {
+    this.todoTitle = '';
     this.todoList = [
       // example of how to make an item in todo list
       { title: 'Install Angular CLI', isDone: false },
@@ -20,5 +22,15 @@ export class AppComponent implements OnInit{
 deleteTodo(todo: any) {
   const index = this.todoList.findIndex(todoItem => todoItem === todo);
   this.todoList.splice(index, 1);
+}
+
+// adds a todo to our list
+addTodo(): void {
+  this.todoList.push({
+    title: this.todoTitle,
+    isDone: false
+  });
+  // resets our todoTitle variable to an empty string
+  this.todoTitle = '';
 }
 }
